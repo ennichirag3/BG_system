68815ef8b6c5d7cf61b50b85c36b11e273fd3e38
Ennichirag <your-email@example.com>
2026-05-15 21:03:02 +0530

    fix bcrypt dependency

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bg-system-3.onrender.com",
+  baseURL: "https://bg-system-3.onrender.com/api",
 });
 
 export default API;
