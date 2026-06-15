88aa82f1fbec647f453687d584a14e4515638adc
Ennichirag <your-email@example.com>
2026-06-15 18:56:25 +0530

    Fix approval page crash

--- client/src/pages/Approval.jsx
+++ client/src/pages/Approval.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import API from "../services/api";
 
